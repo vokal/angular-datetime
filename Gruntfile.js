0b5adbbc3d6bd39421f5199f79c75f71817cc1fb
Devon Maloney <devon.maloney@vokal.io>
2016-01-26 10:11:21 -0600

    republish compile dist folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,31 @@
 "use strict";
 
+var browserifyTransforms = [ [ "babelify", { presets: [ "es2015" ] } ] ];
 module.exports = function ( grunt )
 {
     grunt.initConfig( {
         clean: {
+            build: [ "build/" ],
             coverage: [ "coverage/" ],
             dist: [ "dist/" ]
         },
         less: {
-            all:
-            {
+            all: {
                 files: {
-                    "build/angular-date-picker.css": "source/styles/angular-date-picker.less",
-                    "build/angular-time-picker.css": "source/styles/angular-time-picker.less"
+                    "dist/angular-date-picker.css": "source/styles/angular-date-picker.less",
+                    "dist/angular-time-picker.css": "source/styles/angular-time-picker.less"
                 }
             }
         },
         browserify: {
+            compile: {
+                options: { transform: browserifyTransforms },
+                files: {
+                    "dist/index.js": [ "source/index.js" ],
+                    "dist/angular-date-picker.js": [ "source/date-picker.js" ],
+                    "dist/angular-time-picker.js": [ "source/time-picker.js" ]
+                }
+            },
             test: {
                 options: {
                     transform: [
@@ -28,9 +37,8 @@ module.exports = function ( grunt )
                                 "**/source/time-picker.js",
                                 "**/source/index.js"
                             ]
-                        } ],
-                        [ "babelify", { presets: [ "es2015" ] } ]
-                    ],
+                        } ]
+                    ].concat( browserifyTransforms ),
                     browserifyOptions: {
                         debug: true
                     }
@@ -38,6 +46,15 @@ module.exports = function ( grunt )
                 files: {
                     "build/test.js": [ "test/harness/index.js" ],
                     "build/test-tz.js": [ "test/harness/index-tz.js" ]
+                }
+            }
+        },
+        uglify: {
+            compile: {
+                files: {
+                    "dist/index.min.js": "dist/index.js",
+                    "dist/angular-date-picker.min.js": "dist/angular-date-picker.js",
+                    "dist/angular-time-picker.min.js": "dist/angular-time-picker.js"
                 }
             }
         },
@@ -70,8 +87,10 @@ module.exports = function ( grunt )
     grunt.loadNpmTasks( "grunt-browserify" );
     grunt.loadNpmTasks( "grunt-contrib-clean" );
     grunt.loadNpmTasks( "grunt-contrib-less" );
+    grunt.loadNpmTasks( "grunt-contrib-uglify" );
     grunt.loadNpmTasks( "grunt-protractor-coverage" );
     grunt.loadNpmTasks( "grunt-istanbul" );
 
-    grunt.registerTask( "default", [ "clean", "less", "browserify", "protractor_coverage", "makeReport" ] );
+    grunt.registerTask( "test", [ "clean", "less", "browserify:test", "protractor_coverage", "makeReport" ] );
+    grunt.registerTask( "compile", [ "clean", "less", "browserify:compile", "uglify" ] );
 };
